254744c4c78fd151498a519ea73cee9e96bc52a9
JorgenSchmidt <jo3smith.joe@yandex.ru>
2022-11-12 21:54:10 +0500

    Module "View" has been made

--- GFDirectTasksSolver/MainWindowViewModel.cs
+++ GFDirectTasksSolver/MainWindowViewModel.cs
@@ -4,6 +4,19 @@ namespace GFDirectTasksSolver
 {
     public class MainWindowViewModel : NotifyPropertyChanged
     {
+        #region переменные для взаимодействия с графическими элементами приложения
 
+
+        #endregion
+
+        #region переменные для взаимодействия с основным интерфейсом приложения
+
+
+        #endregion
+
+        #region переменные для обмена данными между моделью и визуальным представлением приложения
+
+
+        #endregion
     }
 }
